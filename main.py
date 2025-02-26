c274eaf1d7959bbf11b3f4dc48536d858fedec88
Andrew Vigil <Andrew.Lee.Vigil@gmail.com>
2025-02-26 12:19:17 -0800

    this updates requirements and adds main and constants

---
+++ main.py
@@ -0,0 +1,13 @@
+# This imports the pygame library and allows us to utilize it
+import pygame
+
+# This imports the module for the constants
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
