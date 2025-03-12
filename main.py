d3745f46620dd82b9290690a889d5b3306a88776
Andrew Vigil <Andrew.Lee.Vigil@gmail.com>
2025-03-12 12:38:50 -0700

    final changes for base of game logic.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from player import *
 from circleshape import *
 from asteroid import *
 from asteroidfield import *
-
+from shot import Shot
 def main():
     #initializes pygame
     pygame.init()
@@ -20,9 +20,11 @@ def main():
     updateable_group = pygame.sprite.Group()
     drawable_group = pygame.sprite.Group()
     asteroids_group = pygame.sprite.Group()
+    shot_group = pygame.sprite.Group()
     Player.containers = (updateable_group, drawable_group)
     Asteroid.containers = (asteroids_group, updateable_group, drawable_group)
     AsteroidField.containers = (updateable_group)
+    Shot.containers = (shot_group, updateable_group, drawable_group)
 
     #basic prints to check run
     print("Starting Asteroids!")
@@ -36,6 +38,7 @@ def main():
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2), PLAYER_RADIUS)
     asteroid_field = AsteroidField()
     asteroid_field.__init__()
+    
     #update method
     while(pygame.get_init()):
 
@@ -51,14 +54,18 @@ def main():
         #draws player on screen
         for thing in drawable_group:
             thing.draw(screen)
-        
+            
         updateable_group.update(dt)
         #detects collision
         for asteroid in asteroids_group:
             if asteroid.collision(player):
                 print("Game over!")
                 return
-            
+            for shot in shot_group:
+                if asteroid.collision(shot):
+                    asteroid.split(asteroids_group)
+                    shot.kill()
+        
         #ticks clock
         dt = clock.tick(60) / 1000
         
